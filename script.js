636199af5b39896ed18b936a4360b007a8924578
Fox1210 <pablogomezmelendez@gmail.com>
2020-11-10 15:08:01 +0100

    primera modificación de la lista 1

--- script.js
+++ script.js
@@ -12,4 +12,30 @@ window.addEventListener("load", onLoad);
 
 function onLoad() {
   console.log("hi");
+  //funcion1 de lista1
+  fun1List1();
+}
+
+//anade las clases en a los elementos span
+function fun1List1() {
+  //salimos hasta la etiqueta "ul"
+  let spanSelcted = document.querySelector(".selected");
+  let elementoPadre = spanSelcted.parentElement.parentElement;
+  let nodeList = elementoPadre.childNodes;
+  console.log(nodeList); //mustar la lista "ul" que posee una lista de etiquetas "il"
+  console.log("*************************************");
+
+  let contador = 1;
+  for (var i = 0; i < nodeList.length; i++) {
+    if (nodeList[i] instanceof HTMLLIElement) {
+      let il = nodeList[i].children;
+      //antes de ser modificado
+      console.log(il[0]);
+      il[0].classList.add("element-" + contador);
+      console.log(il[0]);
+      console.log("");
+      contador++;
+    }
+  }
+  console.log("*************************************");
 }

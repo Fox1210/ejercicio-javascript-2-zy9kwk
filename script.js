4600052196a32884f8597f59775b0d22004e29d1
Fox1210 <pablogomezmelendez@gmail.com>
2020-11-10 16:25:33 +0100

    segunda modificación en la lista 2

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // En este ejercicio deberéis realizar algunos cambios sobre las dos listas incluidas en "index.html".
 // En la primera lista tendréis que hacer lo siguiente:
-//    * Añadid la clase "element-n" a cada "span" de la lista, siendo "n" el número del lugar que ocupa en la lista (por ejemplo el segundo "span" de la lista tendría la clase "element-2"). Para ello, haced uso de los selectores de nodo (parentNode, nextSibling, firstChild...) partiendo del "span" con la clase "selected" siempre.
-//    * Tras añadir las clases, haced uso de "querySelectorAll" para obtener solo los elementos "li" con valor par (teniendo en cuenta que el primer elmento es el 1) y, a continuación, eliminadlos.
+//(HECHO)    * Añadid la clase "element-n" a cada "span" de la lista, siendo "n" el número del lugar que ocupa en la lista (por ejemplo el segundo "span" de la lista tendría la clase "element-2"). Para ello, haced uso de los selectores de nodo (parentNode, nextSibling, firstChild...) partiendo del "span" con la clase "selected" siempre.
+
+//(HECHO)    * Tras añadir las clases, haced uso de "querySelectorAll" para obtener solo los elementos "li" con valor par (teniendo en cuenta que el primer elmento es el 1) y, a continuación, eliminadlos.
 
 //En la segunda lista, que en principio está vacía, deberéis hacer esto:
 //    * Tenéis que generar dentro de esta lista, nodo a nodo, la misma estructura que ha quedado en la primera lista en la que realizastéis los cambios. Tiene que quedar igual, con la misma jerarquía y con las mismas clases, pero con la diferencia de que en vez de elementos "span" deben ser botones. Para replicar los elementos de la primera lista no hace falta que la recorráis, podéis simplemente generar cada elemento "a mano" una detrás de otro (aunque si usáis la lista de referencia para hacer algún tipo de bucle, mejor)
@@ -12,12 +13,15 @@ window.addEventListener("load", onLoad);
 
 function onLoad() {
   console.log("hi");
-  //funcion1 de lista1
-  fun1List1();
+  //modificcacion 1 de la lista1
+  modificacion1List1();
+  //modificcacion 2 de la lista1
+  modificacion2List1();
+  //removeOdd
 }
 
 //anade las clases en a los elementos span
-function fun1List1() {
+function modificacion1List1() {
   //salimos hasta la etiqueta "ul"
   let spanSelcted = document.querySelector(".selected");
   let elementoPadre = spanSelcted.parentElement.parentElement;
@@ -30,12 +34,23 @@ function fun1List1() {
     if (nodeList[i] instanceof HTMLLIElement) {
       let il = nodeList[i].children;
       //antes de ser modificado
-      console.log(il[0]);
+      //console.log(il[0]);
       il[0].classList.add("element-" + contador);
-      console.log(il[0]);
-      console.log("");
+      //DESPUES DE MODIFICAR
+      //console.log(il[0]);
       contador++;
     }
   }
   console.log("*************************************");
+}
+//borra el ejemplo 2 y 4 de la lista 1
+function modificacion2List1() {
+  //salimos hasta la etiqueta "ul"
+  let spanSelcted = document.querySelector(".selected");
+  let elementoPadre = spanSelcted.parentElement.parentElement;
+  let nodeList = elementoPadre.childNodes;
+  console.log(nodeList); //mustar la lista "ul" que posee una lista de etiquetas "il" completa
+  nodeList[3].remove();
+  nodeList[6].remove();
+  console.log(nodeList); //mustar la lista "ul" que posee una lista de etiquetas "il" una vez se ha eliminado los pares
 }
